098b3d82672a142aa3fe1569a796f5fa30a7b28a
Srikanth_M <srikanthMidathapalli@gmail.com>
2021-07-14 13:56:41 +0530

    Day3 practise problems

---
+++ flip/src/coins/Flip.java
@@ -0,0 +1,15 @@
+package coins;
+
+public class Flip {
+	public static void main(String[] args) {
+		if (Math.random()< 0.5)
+		{
+			System.out.println("heads"); 
+			
+		}
+		else
+			System.out.println("tails");
+		
+	}
+
+}
